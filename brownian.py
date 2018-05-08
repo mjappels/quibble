4a29c780f1b3d2da9c0eb21db6fc2c5d2df8acb3
mjappels <mike.appels@gmail.com>
2018-05-08 13:21:30 +0100

    autopeped and fixed plot function

--- brownian.py
+++ brownian.py
@@ -23,16 +23,18 @@ class RandomWalk:
 
 class Brownian(RandomWalk):
     def __init__(self, T=1, n=1000):
-        super().__init__(T*n, 1 / np.sqrt(n), 0.5)
+        super().__init__(T * n, 1 / np.sqrt(n), 0.5)
         self.T = T
         self.n = n
         self.dt = T / n
 
     def plot(self, style='b-'):
-        plt.plot(np.arange(self.n) * self.dt, self.path, style)
+        plt.plot(np.arange(self.T * self.n) * self.dt, self.path, style)
         plt.show()
 
 
 if __name__ == '__main__':
-    a = Brownian(3, 1000)
+    a = RandomWalk(50)
     a.plot()
+    b = Brownian(3, 1000)
+    b.plot()
