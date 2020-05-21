c98e0ff5b12551448d2137c5972deac950486a00
mjappels <mike.appels@gmail.com>
2020-05-22 00:35:37 +0100

    changed get_yield to accept pv as argument instead of discount_curve

--- quibble/bond.py
+++ quibble/bond.py
@@ -18,8 +18,7 @@ class Bond:
 
         return numpy.sum(self._cash_flows * discount_curve(years_to_flow), where=years_to_flow > 0)
 
-    def get_yield(self, date: datetime.date, discount_curve) -> float:
-        face_value = self.pv(date, discount_curve)
+    def get_yield(self, date: datetime.date, pv: float) -> float:
         years_to_flow = numpy.fromiter(
             ((flow_date - date).days / 365 for flow_date in self._cash_flow_dates),
             count=len(self._cash_flow_dates),
@@ -27,6 +26,6 @@ class Bond:
         )
 
         def f(y):
-            return numpy.sum(self._cash_flows * numpy.exp(-y * years_to_flow), where=years_to_flow > 0) - face_value
+            return numpy.sum(self._cash_flows * numpy.exp(-y * years_to_flow), where=years_to_flow > 0) - pv
 
         return scipy.optimize.fsolve(f, 0)[0]
